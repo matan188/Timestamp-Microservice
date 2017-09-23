91bcd89e1c4791eeea8849223e56a5814c83cdd0
Matan <matan.modai@gmail.com>
2017-09-23 18:02:46 +0300

    Timestamp: getDateFromInput fields are now numbers instead of strings

--- test/timestamp.test.js
+++ test/timestamp.test.js
@@ -3,14 +3,70 @@ const expect = require('expect');
 const ts = require('../timestamp');
 
 
-describe("getDateFromInput", () => {
-    it('should create json with correct day, month, year', () => {
+
+
+describe('Test months functions', () => {
+    // getMonthName()
+    it('should return January', () => {
+        expect(ts.getMonthName(0)).toBe('January');
+    });
+
+    it('should return December', () => {
+        expect(ts.getMonthName(11)).toBe('December');
+    });
+
+    it('should return null', () => {
+        expect(ts.getMonthName(12)).toBe(null);
+    });
+
+    // getMonthNumber()
+    it('Test with alternating case', () => {
+        expect(ts.getMonthNumber('decEmbEr')).toBe(11);
+    });
+
+    it('Test with lower case', () => {
+        expect(ts.getMonthNumber('february')).toBe(1);
+    });
+
+    it('Test with alternating case', () => {
+        expect(ts.getMonthNumber('juNE')).toBe(5);
+    });
+
+    it('None valid month', () => {
+        expect(ts.getMonthNumber('incorrect')).toBe(-1);
+    });
+});
+
+
+
+describe('getDateFromInput', () => {
+    var dateJson = {
+        day: 1, 
+        month: 'December',
+        year: 2013 
+    };
+
+    var unixJson = {
+        unix: 123453433
+    };
+
+    it('unix', () => {
+        expect(ts.getDateFromInput('123453433')).toEqual(unixJson);
+    });
+
+    it('Format "month day, year"', () => {
         expect(ts.getDateFromInput('December 1, 2013'))
-            .toEqual({
-                day: '1', 
-                month: 'December',
-                year: '2013' 
-            });
-            
-    }); 
+            .toEqual(dateJson);
+    });
+    
+    it('Format "month day year"', () => {
+        expect(ts.getDateFromInput('December 1 2013'))
+            .toEqual(dateJson);
+    });
+
+    it('Format "year day month"', () => {
+        expect(ts.getDateFromInput('December 1 2013'))
+            .toEqual(dateJson);
+    });
+
 });
--- timestamp.js
+++ timestamp.js
@@ -1,15 +1,17 @@
 var getDateFromInput = function (input) {
     var set = new Set(input.split(' ').map((el) => el.replace(',', '')));
     var inputDate = {};
-    if (set.size > 1) {
+
+    if (set.size > 1) { // natural
         for (let item of set.values()) {
-            if (item.length === 4 && isNaN(item) === false) inputDate.year = item;
+            if (item.length === 4 && isNaN(item) === false) inputDate.year = parseInt(item);
             else if (isNaN(item)) inputDate.month = item;
-            else inputDate.day = item;
+            else inputDate.day = parseInt(item);
         }
-    } else if (set.length === 1) {
-        inputDate.unix = set
-    } else {
+    } else if (set.size === 1) { // unix
+        for (let item of set.values()) inputDate.unix = parseInt(item);
+        
+    } else { // invalid
         return null;
     }
 
@@ -17,6 +19,21 @@ var getDateFromInput = function (input) {
     return inputDate;
 };
 
+var months = (['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+'September', 'October', 'November', 'December']);
+
+var getMonthNumber = (month) => {
+    var lowerMonths = months.map((monthName) => monthName.toLowerCase());
+    return lowerMonths.indexOf(month.toLowerCase());
+}
+
+var getMonthName = (monthNumber) => {
+    if (monthNumber < 0 || monthNumber > 11) return null;
+    return months[monthNumber];
+};
+
 module.exports = {
-    getDateFromInput
+    getDateFromInput,
+    getMonthNumber,
+    getMonthName
 }
